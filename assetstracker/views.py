6a227a5528f4e04424569fa63c1815636262106d
Mahmood Khan <mkhan@mercycorps.org>
2016-12-09 12:56:27 -0800

    added jwt settings and setting jwt cookie based on session in view

--- assetstracker/views.py
+++ assetstracker/views.py
@@ -1,8 +1,12 @@
 from django.views.generic import TemplateView
-
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import response, schemas
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+from rest_framework_jwt.settings import api_settings
+from datetime import datetime
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 @api_view()
 @renderer_classes([OpenAPIRenderer, SwaggerUIRenderer])
@@ -14,8 +18,30 @@ def schema_view(request):
 class HomeView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        print("executing the view code")
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['user'] = "Me me!"
-        return context
+    def render_to_response(self, context, **response_kwargs):
+        response = super(HomeView, self).render_to_response(context, **response_kwargs)
+        response.set_cookie(key="jwt", value=convert_sessionid_to_jwt(self.request.user), domain="localhost")
+        return response
+
+
+def jwt_payload_handler(user):
+    try:
+        username = user.get_username()
+    except AttributeError:
+        username = user.username
+
+    return {
+        'user_id': user.pk,
+        'email': user.email,
+        'username': username,
+        'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA
+    }
+
+def convert_sessionid_to_jwt(user):
+    """convert a session_id to a jwt.
+
+    """
+    payload = jwt_payload_handler(user)
+    return jwt_encode_handler(payload)
+
+
