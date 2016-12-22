866cf81af8c657ff5214b9c9aeee5e820020de57
Mahmood Khan <mkhan@mercycorps.org>
2016-12-21 19:26:11 -0800

    only generate a new jwt token if the user is already authenticated and a token does not already exist.

--- assetstracker/views.py
+++ assetstracker/views.py
@@ -18,9 +18,11 @@ def schema_view(request):
 class HomeView(TemplateView):
     template_name = 'index.html'
 
-    def render_to_response2(self, context, **response_kwargs):
+    def render_to_response(self, context, **response_kwargs):
         response = super(HomeView, self).render_to_response(context, **response_kwargs)
-        response.set_cookie(key="jwt", value=convert_sessionid_to_jwt(self.request.user), domain="localhost")
+        if self.request.user.is_authenticated():
+            if self.request.COOKIES.get("jwt", None) is None:
+                response.set_cookie(key="jwt", value=convert_sessionid_to_jwt(self.request.user), domain="localhost")
         return response
 
 
