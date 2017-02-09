b8373d2e7c3fb1feea3c09c425cbb97ae6d478af
Mahmood Khan <mkhan@mercycorps.org>
2017-02-08 20:55:46 -0800

    added middleware so that i can delay response by a certain amount of time

--- assetsmgmt/settings.py
+++ assetsmgmt/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'assetsmgmt.middleware.DelayResponse',
 ]
 #CORS_ORIGIN_ALLOW_ALL = False
 
