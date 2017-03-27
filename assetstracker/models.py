a8438da486b4fd0940e04f852ac90d4449cc4e83
Mahmood Khan <mkhan@mercycorps.org>
2017-03-26 17:44:12 -0700

    get a models field helptext in template

--- assetstracker/models.py
+++ assetstracker/models.py
@@ -14,6 +14,7 @@ from django.db.models import Q, Sum, Max, Min, Count
 from django.utils import timezone
 from django.utils.timezone import utc
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.functional import curry
 
 from djangocosign.models import UserProfile, Region, Country, Office
 
@@ -158,6 +159,32 @@ class Asset(CommonBaseAbstractModel):
 
     class JSONAPIMeta:
         resource_name = 'assets'
+
+    def __init__(self, *args, **kwargs):
+        # Call the superclass first; it'll create all of the field objects.
+        super(Asset, self).__init__(*args, **kwargs)
+
+        # Again, iterate over all of our field objects.
+        for field in self._meta.fields:
+            # Create a string, get_FIELDNAME_help text
+            method_name = "get_{0}_help_text".format(field.name)
+
+            # We can use curry to create the method with a pre-defined argument
+            curried_method = curry(self._get_help_text, field_name=field.name)
+
+            # And we add this method to the instance of the class.
+            setattr(self, method_name, curried_method)
+
+            # You can then use it in template like this:
+            # <span>{{ asset.get_assettype_help_text }}</span>
+
+
+    def _get_help_text(self, field_name):
+        """Given a field name, return it's help text."""
+        for field in self._meta.fields:
+            if field.name == field_name:
+                return field.help_text
+
 
 @python_2_unicode_compatible
 class AssetIssuanceHistory(CommonBaseAbstractModel):
